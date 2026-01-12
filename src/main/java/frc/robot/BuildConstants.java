84151c11bc3d9b56d537c3901c0af7ce8e4c55c5
omniscient-cookie <vainsencher.or@gmail.com>
2026-01-12 15:43:32 -0500

    Initial turret commit

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2026-01-12 00:23:28 EST";
-  public static final long BUILD_UNIX_TIME = 1768195408487L;
-  public static final int DIRTY = 129;
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "21d549ebfa0208339ff89cfd3e7ba047d855fd12";
+  public static final String GIT_DATE = "2026-01-12 00:25:49 EST";
+  public static final String GIT_BRANCH = "turret";
+  public static final String BUILD_DATE = "2026-01-12 15:21:36 EST";
+  public static final long BUILD_UNIX_TIME = 1768249296731L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
---
+++ src/main/java/frc/robot/subsystems/turret/TurretIO.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems.turret;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface TurretIO {
+    @AutoLog
+    public static class TurretIOInputs {
+        // TODO PUT INPUTS HERE
+    }
+
+    
+}
