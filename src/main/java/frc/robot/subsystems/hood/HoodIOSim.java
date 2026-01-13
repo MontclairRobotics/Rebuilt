90a23f9f62d71457be82c205f34151421746654d
rafaelbaird <155581003+rafaelbaird@users.noreply.github.com>
2026-01-13 16:53:45 -0500

    Hood code from 1/13/25 meeting

---
+++ src/main/java/frc/robot/subsystems/hood/HoodIOSim.java
@@ -0,0 +1,94 @@
+package frc.robot.subsystems.hood;
+
+import static edu.wpi.first.units.Units.Radians;
+import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecondPerSecond;
+
+import java.util.function.DoubleSupplier;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import frc.robot.constants.HoodConstants;
+import frc.robot.subsystems.hood.HoodIO.HoodIOInputs;
+
+public class HoodIOSim implements HoodIO {
+
+	public DCMotor motor;
+	public ProfiledPIDController pidController;
+	public ArmFeedforward feedforward;
+	public SingleJointedArmSim sim;
+	
+	public HoodIOSim() {
+		motor = DCMotor.getKrakenX44(1);
+		// sim = new SingleJointedArmSim(
+		// 	motor, 
+		// 	HoodConstants.GEAR_RATIO, 
+		// 	HoodConstants.MOMENT_OF_INERTIA, 
+		// 	getAngle(), 
+		// 	getAngle(), 
+		// 	getAngle(), 
+		// 	true, 
+		// 	getAngle(), 
+		// 	null)
+
+		pidController = new ProfiledPIDController(
+			0, 
+			0, 
+			0, 
+			new Constraints(HoodConstants.MAX_VELOCITY.in(RotationsPerSecond), 
+			HoodConstants.MAX_ACCELERATION.in(RotationsPerSecondPerSecond))
+		);
+
+		feedforward = new ArmFeedforward(0, 0 , 0);
+	}
+
+	@Override
+	public void updateInputs(HoodIOInputs inputs) {
+		inputs.appliedVoltage = 0;
+		// inputs.current = motor.getStatorCurrent().getValueAsDouble();
+		// inputs.tempCelsius = motor.getDeviceTemp().getValueAsDouble();
+		inputs.angle = getAngle();
+		inputs.angleSetpoint = pidController.getSetpoint().position;
+	}
+
+	@Override
+	public void setVoltage(double voltage) {
+		// motor.setVoltage(voltage);
+	}
+
+	// @Override
+	// public double getAngle() {
+	// 	// return motor.getPosition().getValueAsDouble();
+	// }
+
+	@Override
+	public void setAngle(double goal) {
+		double pidVoltage = pidController.calculate(getAngle(), goal);
+		double feedforwardVoltage = feedforward.calculate(Rotations.of(getAngle()).in(Radians), 0);
+		setVoltage(pidVoltage + feedforwardVoltage);
+	}
+
+	@Override
+	public void setAngle(DoubleSupplier goalSupplier) {
+		double currentGoal = goalSupplier.getAsDouble();
+		setAngle(currentGoal);
+	}
+
+	// @Override
+	// public void stop() {
+	// 	motor.stopMotor();
+	// }
+
+	@Override
+	public boolean atSetpoint() {
+		return pidController.atGoal();
+	}
+
+}
