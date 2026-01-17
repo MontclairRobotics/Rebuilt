1d207d0cc56430ccf1d54165ce584fd9b10e547f
rafaelbaird <155581003+rafaelbaird@users.noreply.github.com>
2026-01-16 21:44:52 -0500

    Added vision2, Vision using AdvantageKit's template
    Went through and read every file to understand. They are doing a couple of things that are great:
    - better logging through io layers
    - logging is separate from class logic, makes everything a TON more readable
    - no stupid bullshit periodic code
    - one vision object with multiple cameras that can be anything (sim, limelight, limelight without interal imu)
    -

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 7;
-  public static final String GIT_SHA = "2e3c5b3bb2e6565c2f2ce9754290f5c6c3190222";
-  public static final String GIT_DATE = "2026-01-14 15:18:16 EST";
+  public static final int GIT_REVISION = 9;
+  public static final String GIT_SHA = "be3260468bd09d31abef19d565801b59d8451f18";
+  public static final String GIT_DATE = "2026-01-15 16:26:37 EST";
   public static final String GIT_BRANCH = "vision";
-  public static final String BUILD_DATE = "2026-01-14 16:26:24 EST";
-  public static final long BUILD_UNIX_TIME = 1768425984934L;
+  public static final String BUILD_DATE = "2026-01-16 19:57:06 EST";
+  public static final long BUILD_UNIX_TIME = 1768611426692L;
   public static final int DIRTY = 0;
 
   private BuildConstants() {}
