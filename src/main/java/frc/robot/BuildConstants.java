c2ce782ddc4a2e3387dbdfd9c896ce84e806aabf
rafaelbaird <155581003+rafaelbaird@users.noreply.github.com>
2026-01-20 14:12:18 -0500

    Configured testing robot CAD model for AdvantageKit
    Ready for testing of hood and turret!

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -6,11 +6,11 @@ public final class BuildConstants {
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
   public static final int GIT_REVISION = 2;
-  public static final String GIT_SHA = "777aff5d0e5c601f029d121db88dd0b81215dc9b";
-  public static final String GIT_DATE = "2026-01-17 16:36:05 EST";
-  public static final String GIT_BRANCH = "maple-sim";
-  public static final String BUILD_DATE = "2026-01-18 22:14:06 EST";
-  public static final long BUILD_UNIX_TIME = 1768792446181L;
+  public static final String GIT_SHA = "58d38d2a03c4fa8c3660c3eb281b63ca5803d92c";
+  public static final String GIT_DATE = "2026-01-20 13:53:51 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-01-20 14:02:15 EST";
+  public static final long BUILD_UNIX_TIME = 1768935735669L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
