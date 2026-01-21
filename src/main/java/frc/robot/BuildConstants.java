2315413a3d200bd263dfb6ecba415fd8e6152256
Omega314OB <brackenburyoj@gmail.com>
2026-01-21 16:19:23 -0500

    Added Flywheel Objects in Robot Container
    Added flywheel real and sim objects in the robot container

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 6;
-  public static final String GIT_SHA = "97b9d705647103b7236e9fa1c30dedbc35381587";
-  public static final String GIT_DATE = "2026-01-20 16:38:57 EST";
+  public static final int GIT_REVISION = 21;
+  public static final String GIT_SHA = "d38356cf5d5487a6b92686c6e2fd359dccde3f8a";
+  public static final String GIT_DATE = "2026-01-21 16:15:58 EST";
   public static final String GIT_BRANCH = "flywheel";
-  public static final String BUILD_DATE = "2026-01-21 15:36:59 EST";
-  public static final long BUILD_UNIX_TIME = 1769027819348L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-01-21 16:16:15 EST";
+  public static final long BUILD_UNIX_TIME = 1769030175972L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
--- src/main/java/frc/robot/subsystems/turret/TurretIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/turret/TurretIOTalonFX.java
@@ -1,49 +1,50 @@
 package frc.robot.subsystems.turret;
 
+import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecondPerSecond;
+import static frc.robot.constants.TurretConstants.*;
+
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import frc.robot.RobotContainer;
-import frc.robot.constants.TurretConstants;
-
-import static edu.wpi.first.units.Units.RadiansPerSecondPerSecond;
-import static edu.wpi.first.units.Units.Rotations;
-import static edu.wpi.first.units.Units.RotationsPerSecond;
-import static edu.wpi.first.units.Units.RotationsPerSecondPerSecond;
-import static frc.robot.constants.TurretConstants.*;
 import java.util.function.DoubleSupplier;
 
 public class TurretIOTalonFX implements TurretIO {
 
-    private TalonFX motor;
-    private MotionMagicVoltage mm_req;
-	private double robotRelativeSetpoint;
-	private double fieldRelativeSetpoint;
-    public TurretIOTalonFX() {
+  private TalonFX motor;
+  private MotionMagicVoltage mm_req;
+  private double robotRelativeSetpoint;
+  private double fieldRelativeSetpoint;
 
-		robotRelativeSetpoint = 0;
-		fieldRelativeSetpoint = 0;
+  public TurretIOTalonFX() {
 
-		motor = new TalonFX(CAN_ID);
-		TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();
-		Slot0Configs slot0Configs = talonFXConfigs.Slot0;
+    robotRelativeSetpoint = 0;
+    fieldRelativeSetpoint = 0;
 
-		slot0Configs.kS = TALON_KS;
-		slot0Configs.kV = TALON_KV;
-		slot0Configs.kA = TALON_KA;
-		slot0Configs.kP = TALON_KP;
-		slot0Configs.kI = TALON_KI;
-		slot0Configs.kD = TALON_KD;
+    motor = new TalonFX(CAN_ID);
+    TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();
+    Slot0Configs slot0Configs = talonFXConfigs.Slot0;
 
-		mm_req = new MotionMagicVoltage(0);
+    slot0Configs.kS = TALON_KS;
+    slot0Configs.kV = TALON_KV;
+    slot0Configs.kA = TALON_KA;
+    slot0Configs.kP = TALON_KP;
+    slot0Configs.kI = TALON_KI;
+    slot0Configs.kD = TALON_KD;
 
-		MotionMagicConfigs motionMagicConfigs = talonFXConfigs.MotionMagic;
-		motionMagicConfigs.MotionMagicCruiseVelocity = TURRET_CRUISE_VELOCITY.in(RotationsPerSecond)*GEAR_RATIO;
-		motionMagicConfigs.MotionMagicAcceleration = TURRET_ACCELERATION.in(RotationsPerSecondPerSecond)*GEAR_RATIO; 
-		motionMagicConfigs.MotionMagicJerk = TURRET_JERK*GEAR_RATIO;
-		motor.getConfigurator().apply(talonFXConfigs);
+    mm_req = new MotionMagicVoltage(0);
+
+    MotionMagicConfigs motionMagicConfigs = talonFXConfigs.MotionMagic;
+    motionMagicConfigs.MotionMagicCruiseVelocity =
+        TURRET_CRUISE_VELOCITY.in(RotationsPerSecond) * GEAR_RATIO;
+    motionMagicConfigs.MotionMagicAcceleration =
+        TURRET_ACCELERATION.in(RotationsPerSecondPerSecond) * GEAR_RATIO;
+    motionMagicConfigs.MotionMagicJerk = TURRET_JERK * GEAR_RATIO;
+    motor.getConfigurator().apply(talonFXConfigs);
   }
 
   @Override
@@ -58,9 +59,14 @@ public class TurretIOTalonFX implements TurretIO {
 
   @Override
   public void setRobotRelativeAngle(double angle) {
-	robotRelativeSetpoint = angle;
-     // converts to motor output shaft rotations and makes request to motion magic for a new setpoint.
-    motor.setControl(mm_req.withPosition(wrapAngleSetpoint(angle)*GEAR_RATIO).withSlot(0).withEnableFOC(false));
+    robotRelativeSetpoint = angle;
+    // converts to motor output shaft rotations and makes request to motion magic for a new
+    // setpoint.
+    motor.setControl(
+        mm_req
+            .withPosition(wrapAngleSetpoint(angle) * GEAR_RATIO)
+            .withSlot(0)
+            .withEnableFOC(false));
   }
 
   @Override
@@ -90,8 +96,9 @@ public class TurretIOTalonFX implements TurretIO {
 
   @Override
   public void setFieldRelativeAngle(double angle) {
-	fieldRelativeSetpoint = angle;
-    double robotRelativeAngleSetpoint = angle - RobotContainer.drivetrain.odometryHeading.getRotations();
+    fieldRelativeSetpoint = angle;
+    double robotRelativeAngleSetpoint =
+        angle - RobotContainer.drivetrain.odometryHeading.getRotations();
     setRobotRelativeAngle(robotRelativeAngleSetpoint);
   }
 
@@ -102,22 +109,24 @@ public class TurretIOTalonFX implements TurretIO {
 
   @Override
   public double getFieldRelativeAngle() {
-    return getRobotRelativeAngle()+RobotContainer.drivetrain.odometryHeading.getRotations();
+    return getRobotRelativeAngle() + RobotContainer.drivetrain.odometryHeading.getRotations();
   }
 
   @Override
   public boolean atSetpoint() {
-    return Math.abs(getRobotRelativeAngle() - mm_req.getPositionMeasure().in(Rotations)/GEAR_RATIO) < ANGLE_TOLERANCE.in(Rotations);
+    return Math.abs(
+            getRobotRelativeAngle() - mm_req.getPositionMeasure().in(Rotations) / GEAR_RATIO)
+        < ANGLE_TOLERANCE.in(Rotations);
   }
 
   @Override
   public double wrapAngleSetpoint(double angle) {
-		if(angle>MAX_ANGLE.in(Rotations)) {
-			return angle-1;
-		}else if(angle<0){
-			return angle+1;
-		}else {
-			return angle;
-		}
-	}
+    if (angle > MAX_ANGLE.in(Rotations)) {
+      return angle - 1;
+    } else if (angle < 0) {
+      return angle + 1;
+    } else {
+      return angle;
+    }
+  }
 }
