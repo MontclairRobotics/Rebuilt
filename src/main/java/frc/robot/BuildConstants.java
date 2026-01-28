1d42c57aa77e58f5be98dac046ebbf06af84cb61
rafaelbaird <155581003+rafaelbaird@users.noreply.github.com>
2026-01-28 12:17:06 -0500

    Turret completely re-factored, added turret OFFSET

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 33;
-  public static final String GIT_SHA = "79b3f32fe004e2a61cbd7e02f07e79038b5c3729";
-  public static final String GIT_DATE = "2026-01-27 15:25:31 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-27 23:36:13 EST";
-  public static final long BUILD_UNIX_TIME = 1769574973819L;
+  public static final int GIT_REVISION = 34;
+  public static final String GIT_SHA = "d29211dea4163f6bd0cdaae1678ccdb70fc243de";
+  public static final String GIT_DATE = "2026-01-27 23:39:28 EST";
+  public static final String GIT_BRANCH = "turret-refactoring";
+  public static final String BUILD_DATE = "2026-01-28 12:10:52 EST";
+  public static final long BUILD_UNIX_TIME = 1769620252064L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
