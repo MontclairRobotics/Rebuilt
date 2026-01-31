58af2333c5aa32e5bda48a6e394236cfbc37bd0e
sub0dev <125705137+Mr-Pyro@users.noreply.github.com>
2026-01-31 10:31:47 -0500

    superduper

--- src/main/java/frc/robot/superstructure/Modes.java
+++ src/main/java/frc/robot/superstructure/Modes.java
@@ -1,37 +1,52 @@
 package frc.robot.superstructure;
 
-import frc.robot.subsystems.turret.Turret;
-import frc.robot.util.FieldConstants; 
+import edu.wpi.first.math.geometry.Translation2d;
 
-public enum Modes{
-    SCORING,
-    CLOSE_FERRYING_LEFT,
-    CLOSE_FERRYING_RIGHT,
-    FAR_FERRYING_LEFT,
-    FAR_FERRYING_RIGHT,
-    NO_ZONE;
-}
+public class Superstructure {
 
-public void Switch(){
-if (Turret.getFieldRelativePosition().getX() <= FieldConstants.LinesHorizontal.starting){ 
-    Modes = Modes.SCORING;   
-}
-else if(Turret.getFieldRelativePosition().getX() <= FieldConstants.LinesHorizontal.neutralZoneNear){
-    Modes = Modes.NO_ZONE;
-}
-else if (Turret.getFieldRelativePosition().getY() <= FieldConstants.LinesVertical.center && Turret.getFieldRelativePosition().getX() <= FieldConstants.LinesHorizontal.center){
-    Modes = Modes.CLOSE_FERRYING_RIGHT;
-}
-else if (Turret.getFieldRelativePosition().getY() >= FieldConstants.LinesVertical.center && Turret.getFieldRelativePosition().getX() <= FieldConstants.LinesHorizontal.center){
-    Modes = Modes.CLOSE_FERRYING_LEFT;
-}
-else if (Turret.getFieldRelativePosition().getY() <= FieldConstants.LinesVertical.center && Turret.getFieldRelativePosition().getX() <= FieldConstants.LinesHorizontal.neutralZoneFar){
-    Modes = Modes.FAR_FERRYING_RIGHT;
-}
-else if (Turret.getFieldRelativePosition().getY() >= FieldConstants.LinesVertical.center && Turret.getFieldRelativePosition().getX() <= FieldConstants.LinesHorizontal.neutralZoneFar){
-    Modes = Modes.FAR_FERRYING_LEFT;
-}
-else{
-    Modes = Modes.NO_ZONE;
-}
+    public enum Modes {
+        SCORING,
+        CLOSE_FERRYING_LEFT,
+        CLOSE_FERRYING_RIGHT,
+        FAR_FERRYING_LEFT,
+        FAR_FERRYING_RIGHT,
+        NO_ZONE
+    }
+
+    private Modes currentMode = Modes.NO_ZONE;
+
+    public void switchMode() {
+
+        Translation2d pos = Turret.getFieldRelativePosition();
+
+        if (pos.getX() <= FieldConstants.LinesHorizontal.starting) { 
+            currentMode = Modes.SCORING;   
+        }
+        else if (pos.getX() <= FieldConstants.LinesHorizontal.neutralZoneNear) {
+            currentMode = Modes.NO_ZONE;
+        }
+        else if (pos.getY() <= FieldConstants.LinesVertical.center &&
+                 pos.getX() <= FieldConstants.LinesHorizontal.center) {
+            currentMode = Modes.CLOSE_FERRYING_RIGHT;
+        }
+        else if (pos.getY() >= FieldConstants.LinesVertical.center &&
+                 pos.getX() <= FieldConstants.LinesHorizontal.center) {
+            currentMode = Modes.CLOSE_FERRYING_LEFT;
+        }
+        else if (pos.getY() <= FieldConstants.LinesVertical.center &&
+                 pos.getX() <= FieldConstants.LinesHorizontal.neutralZoneFar) {
+            currentMode = Modes.FAR_FERRYING_RIGHT;
+        }
+        else if (pos.getY() >= FieldConstants.LinesVertical.center &&
+                 pos.getX() <= FieldConstants.LinesHorizontal.neutralZoneFar) {
+            currentMode = Modes.FAR_FERRYING_LEFT;
+        }
+        else {
+            currentMode = Modes.NO_ZONE;
+        }
+    }
+
+    public Modes getCurrentMode() {
+        return currentMode;
+    }
 }
