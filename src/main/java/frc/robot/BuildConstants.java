25a39cd48339ad504cc55ddc888b545d480b6b2d
Jame <jctee02@gmail.com>
2026-02-04 16:51:53 -0500

    Pushing AutoChooser
    Now back to bed

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "21d549ebfa0208339ff89cfd3e7ba047d855fd12";
-  public static final String GIT_DATE = "2026-01-12 00:25:49 EST";
+  public static final int GIT_REVISION = 29;
+  public static final String GIT_SHA = "24ab11c246b142b90f47d393e9ed4341e6aa2036";
+  public static final String GIT_DATE = "2026-01-27 15:53:36 EST";
   public static final String GIT_BRANCH = "auto";
-  public static final String BUILD_DATE = "2026-01-13 14:43:03 EST";
-  public static final long BUILD_UNIX_TIME = 1768333383543L;
+  public static final String BUILD_DATE = "2026-02-03 16:15:33 EST";
+  public static final long BUILD_UNIX_TIME = 1770153333860L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/util/Auto.java
+++ src/main/java/frc/robot/util/Auto.java
@@ -1,5 +1,21 @@
 package frc.robot.util;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.Command;
+
 public class Auto {
-    
+  public final SendableChooser<Command> autoChooser = AutoBuilder.buildAutoChooser();
+  
+  SmartDashboard.putData("Auto Chooser", autoChooser);
+  
+  NetworkTableInstance instAuto = NetworkTableInstance.getDefault();
+  NetworkTable auto = instAuto.getTable("Auto");
+  
+  public Command getAutonomousCommand() {
+    return autoChooser.getSelected();
+  }
 }
