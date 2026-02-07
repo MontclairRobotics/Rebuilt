6e9e4c559f9b26de55618a223153cac2f879b08b
sub0dev <125705137+Mr-Pyro@users.noreply.github.com>
2026-02-07 10:07:45 -0500

    pluto(superman's dog)

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 56;
-  public static final String GIT_SHA = "5598ad17d1cb89175be6d0b334d9f2ca5f5800fd";
-  public static final String GIT_DATE = "2026-02-03 15:41:50 EST";
+  public static final int GIT_REVISION = 57;
+  public static final String GIT_SHA = "746df5a4fcf3698d73a25ec6774c125594b07f5d";
+  public static final String GIT_DATE = "2026-02-04 16:10:36 EST";
   public static final String GIT_BRANCH = "superstructure";
-  public static final String BUILD_DATE = "2026-02-04 15:37:28 EST";
-  public static final long BUILD_UNIX_TIME = 1770237448581L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-02-07 10:04:47 EST";
+  public static final long BUILD_UNIX_TIME = 1770476687639L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
--- src/main/java/frc/robot/constants/SpindexerConstants.java
+++ src/main/java/frc/robot/constants/SpindexerConstants.java
@@ -7,7 +7,7 @@ public class SpindexerConstants {
   public static final Slot0Configs SLOT0_CONFIGS =
       new Slot0Configs().withKP(0).withKI(0).withKD(0).withKS(0).withKV(0).withKA(0).withKG(0);
 
-  public static final double MOMENT_OF_INERTIA = 0;
+  public static final double MOMENT_OF_INERTIA = 0.1;//TODO:fix this i guess
   public static final double GEARING = 1;
   public static final double SPIN_VOLTAGE = 0.0;
 }
