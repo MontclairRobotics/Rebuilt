a562afb83341905c915e063734a1a71c3f747290
rafaelbaird <155581003+rafaelbaird@users.noreply.github.com>
2026-02-09 17:50:20 -0500

    Update BuildConstants.java for the main branch

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 73;
-  public static final String GIT_SHA = "aab9fcdec775676216c7e6560a3fc779c619205d";
-  public static final String GIT_DATE = "2026-02-07 10:55:20 EST";
-  public static final String GIT_BRANCH = "superstructure";
-  public static final String BUILD_DATE = "2026-02-09 15:21:43 EST";
-  public static final long BUILD_UNIX_TIME = 1770668503724L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 81;
+  public static final String GIT_SHA = "c19b2dfeb734efe35fb73bd2f7a26a463377b9b7";
+  public static final String GIT_DATE = "2026-02-09 16:07:52 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-02-09 17:46:09 EST";
+  public static final long BUILD_UNIX_TIME = 1770677169512L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
