5e585637b78fb81f268a5738a1925493dcbd901c
iris <irisheadquarters01@gmail.com>
2026-02-10 15:43:19 -0500

    move the friggin intake
    moved the intake to th3 correct position

--- src/main/java/frc/robot/subsystems/pivot/PivotVisualization.java
+++ src/main/java/frc/robot/subsystems/pivot/PivotVisualization.java
@@ -11,14 +11,15 @@ import org.littletonrobotics.junction.Logger;
 public class PivotVisualization {
   // pivot 3d pose (x, y, z pos and rotations)
   public Pose3d pivotPose = new Pose3d();
+  public Pose3d correctPivotPose = new Pose3d();
 
   // update current place (always middle of robot) plus the rotation OF the intake pose
   public void update() {
     pivotPose =
         new Pose3d(
+            0.2664714,
             0,
-            0,
-            0,
+            0.1711706,
             new Rotation3d(
                 Angle.ofBaseUnits(0, Rotations),
                 RobotContainer.pivot.io.getPivotAngle(), //turns upwards
