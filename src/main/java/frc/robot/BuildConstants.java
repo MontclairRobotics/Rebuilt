d9bb15d3393e2d6321ba01cd5a316c4650040208
iris <irisheadquarters01@gmail.com>
2026-02-10 15:32:38 -0500

    Update PivotVisualization

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,15 +5,15 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Rebuilt";
+  public static final String MAVEN_NAME = "CODEISHERE";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 81;
-  public static final String GIT_SHA = "c19b2dfeb734efe35fb73bd2f7a26a463377b9b7";
-  public static final String GIT_DATE = "2026-02-09 16:07:52 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-02-09 17:46:09 EST";
-  public static final long BUILD_UNIX_TIME = 1770677169512L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = -1;
+  public static final String GIT_SHA = "UNKNOWN";
+  public static final String GIT_DATE = "UNKNOWN";
+  public static final String GIT_BRANCH = "UNKNOWN";
+  public static final String BUILD_DATE = "2026-02-10 15:21:14 EST";
+  public static final long BUILD_UNIX_TIME = 1770754874403L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
