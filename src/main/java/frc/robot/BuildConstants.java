6f381a102aa078fc746f675d296c70fbb5ae84e3
Montclair Code Team <869c568+cbgpenguin@users.noreply.github.com>
2026-02-14 10:11:43 -0500

    Update Vendor Dependencies and Spindexer IDs

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 128;
-  public static final String GIT_SHA = "e531417eac56d119f88f5565b444f1a6209fdb12";
-  public static final String GIT_DATE = "2026-02-13 21:54:15 EST";
+  public static final int GIT_REVISION = 129;
+  public static final String GIT_SHA = "951b14eedea07f7d06a96ea79a7d8cfb030205d4";
+  public static final String GIT_DATE = "2026-02-14 10:08:18 EST";
   public static final String GIT_BRANCH = "testing-2-14";
-  public static final String BUILD_DATE = "2026-02-14 10:06:15 EST";
-  public static final long BUILD_UNIX_TIME = 1771081575265L;
+  public static final String BUILD_DATE = "2026-02-14 10:11:07 EST";
+  public static final long BUILD_UNIX_TIME = 1771081867252L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/constants/SpindexerConstants.java
+++ src/main/java/frc/robot/constants/SpindexerConstants.java
@@ -1,8 +1,8 @@
 package frc.robot.constants;
 
 public class SpindexerConstants {
-  public static final int SPIN_ID = -1; //TODO: GET MOTOR IDS
-  public static final int INDEX_ID = -1;
+  public static final int SPIN_ID = 20; //TODO: GET MOTOR IDS
+  public static final int INDEX_ID = 21;
 
   public static final double SPIN_MOMENT_OF_INERTIA = 2; //TODO: Get all of these
   public static final double INDEX_MOMENT_OF_INERTIA = 2;
