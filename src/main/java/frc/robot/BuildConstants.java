951b14eedea07f7d06a96ea79a7d8cfb030205d4
rafaelbaird <155581003+rafaelbaird@users.noreply.github.com>
2026-02-14 10:08:18 -0500

    Added Tuner Constants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 127;
-  public static final String GIT_SHA = "fa3d3f0ba6e52101a54e3c6670a0a92feda658fe";
-  public static final String GIT_DATE = "2026-02-12 16:42:18 EST";
+  public static final int GIT_REVISION = 128;
+  public static final String GIT_SHA = "e531417eac56d119f88f5565b444f1a6209fdb12";
+  public static final String GIT_DATE = "2026-02-13 21:54:15 EST";
   public static final String GIT_BRANCH = "testing-2-14";
-  public static final String BUILD_DATE = "2026-02-13 21:51:31 EST";
-  public static final long BUILD_UNIX_TIME = 1771037491473L;
+  public static final String BUILD_DATE = "2026-02-14 10:06:15 EST";
+  public static final long BUILD_UNIX_TIME = 1771081575265L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
