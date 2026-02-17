8960d6e8a6e6b9cecf505ef9b83a8236be9bb335
iris <irisheadquarters01@gmail.com>
2026-02-17 16:52:16 -0500

    Deleted thing

--- src/main/java/frc/robot/subsystems/intake/rollers/Rollers.java
+++ src/main/java/frc/robot/subsystems/intake/rollers/Rollers.java
@@ -5,7 +5,6 @@ import org.littletonrobotics.junction.Logger;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.rollers.RollersIOInputsAutoLogged;
 
 import static frc.robot.constants.RollersConstants.INTAKE_VOLTAGE;
 
