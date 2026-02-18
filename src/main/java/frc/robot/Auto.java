3c29d94edf9a02655d61a4d27257d359779c9a9b
Bry <155580880+BCODERW@users.noreply.github.com>
2026-02-18 17:23:29 -0500

    More spaghetti
    Closed-ish to finishing spaghetti Auto - just added more logic to auto builder and auto validator method and added calcMaxScore method

--- src/main/java/frc/robot/Auto.java
+++ src/main/java/frc/robot/Auto.java
@@ -4,6 +4,7 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.path.PathPoint;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -13,43 +14,64 @@ import java.util.ArrayList;
 public class Auto extends SubsystemBase {
   private String[] pathNames =
       new String[] {
-        "D0", "D1", "D2", "L0", "L1", "L2", "R0", "L0", "O0", "O1", "O2", "O3", "R0", "R1", "R2"
+        "LD0", "LD1", "LD2", "LL0", "LL1", "LL2", "RO0", "RO1", "RO2", "RO3", "RR0", "RR1", "RR2", "LR0", "RL0"
       };
-  private char[] pos = new char[] {'L', 'R', 'C', 'O', 'D'};
   private char currentPos;
   private String autoString = "";
   private ArrayList<PathPlannerPath> pathList = new ArrayList<PathPlannerPath>();
   private ArrayList<Pose2d> posesList = new ArrayList<Pose2d>();
   private ArrayList<PathPoint> pathPointsList = new ArrayList<PathPoint>();
 
-  private Command currentCommand = Commands.none();
+  public Auto() {
+    //Ex auto: LL0L1D0
+  }
 
-  public Auto() {}
+  public int calculateMaxScore(String autoString) {
+    int estimatedScore = 0;
+    if(! isAutoValid(autoString)) {
+      return - 1;
+    }
+
+    if(autoString.substring(0,2) != "LR" && autoString.substring(0,2) != "RL") {
+      estimatedScore += 15; //Assume we collect as much fuel as we can score at for every path + get all fuel in. Change 15 to the actual max fuel storage number
+    }
+
+    for(int i = 3; i < autoString.length(); i += 2) {
+      if(autoString.substring(i,i + 2) != "LR" && autoString.substring(i,i + 2) != "RL") {
+        estimatedScore += 15; //Assume we collect as much fuel as we can score at for every path + get all fuel in. Change 15 to the actual max fuel storage number
+      }
+    }
+
+    return estimatedScore;
+  }
 
   public boolean isAutoValid(String autoString) {
     if (autoString.length() < 5 || autoString.length() % 2 == 0) {
       return false;
     }
 
-    currentPos = autoString.charAt(0);
-    for (int i = 2; i < autoString.length(); i += 2) {
-      if (autoString.charAt(i) != currentPos && autoString.charAt(i) != 'C') {
-        return false;
+    String currentAutoString = autoString.substring(0,3);
+    boolean doesPathExist = false;
+    for(int i = 0; i < autoString.length(); i++) {
+      if(currentAutoString == pathNames[i]) {
+        doesPathExist = true;
       }
     }
 
-    String currentPath;
-    boolean pathExists;
-    for (int i = 2; i < autoString.length(); i += 2) {
-      currentPath = autoString.substring(i, i + 3);
-      pathExists = false;
-      for (int j = 0; j < pathNames.length; j++) {
-        if (pathNames[j] == currentPath) {
-          pathExists = true;
+    if(! doesPathExist) {
+      return false;
+    }
+
+    for(int i = 3; i < autoString.length(); i += 2) {
+      currentAutoString = currentAutoString.charAt(1) + autoString.substring(i, i + 2);
+      doesPathExist = false;
+      for(int j = 0; j < pathNames.length; j++) {
+        if(currentAutoString == pathNames[j]) {
+          doesPathExist = true;
         }
       }
 
-      if (!pathExists) {
+      if(! doesPathExist) {
         return false;
       }
     }
@@ -57,17 +79,43 @@ public class Auto extends SubsystemBase {
     return true;
   }
 
+  //Doesn't work for both alliances and doesn't account for ferrying paths
   public Command buildAuto(String autoString) {
     if (!isAutoValid(autoString)) {
-      return null;
+      return null; //TODO: defalt
     }
 
     SequentialCommandGroup autoCommand = new SequentialCommandGroup();
 
     currentPos = autoString.charAt(1);
     try {
-      autoCommand.addCommands(
-          AutoBuilder.followPath(PathPlannerPath.fromPathFile(autoString.substring(0, 3))));
+      if(currentPos != 'D' || autoString.charAt(2) == '0' && currentPos != 'O' || autoString.charAt(2) == '0') {
+        autoCommand.addCommands(
+          RobotContainer.pivot.stowCommand(),
+          Commands.race(
+            AutoBuilder.followPath(PathPlannerPath.fromPathFile(autoString.substring(0, 3))),
+            RobotContainer.rollers.intakeCommand() //IDK how they have implemented scoring while dirving so this is a placeholder
+          )
+        );
+
+        if(autoString.charAt(1) == 'L' || autoString.charAt(1) == 'R') {
+          autoCommand.addCommands(
+            RobotContainer.pivot.unstowCommand(),
+            RobotContainer.shooter.indexAndShootCommand(() -> Units.RadiansPerSecond.of(0.0)).withTimeout(6.0) //Change 6 secs to how much time it takes to shoot all fuel plus change the flywheel speed
+          );
+        }
+      } else {
+        autoCommand.addCommands(
+          RobotContainer.pivot.unstowCommand(),
+          Commands.race(PathPlannerPath.fromPathFile(autoString.substring(0, 3)),
+            Commands.sequence(
+              Commands.waitUntil(/*our x position is < a certain amount*/),
+              RobotContainer.shooter.whileScoring() //IDK how they have implemented scoring while dirving so this is a placeholder
+            ),
+            RobotContainer.rollers.intakeCommand()
+          )
+        );
+      }
     } catch (Exception e) {
 
     }
@@ -75,9 +123,17 @@ public class Auto extends SubsystemBase {
     for (int i = 3; i < autoString.length(); i += 2) {
       try {
         autoCommand.addCommands(
-            AutoBuilder.followPath(
-                PathPlannerPath.fromPathFile(currentPos + autoString.substring(i, i + 2))));
-        currentPos = autoString.charAt(i + 1);
+          RobotContainer.pivot.stowCommand(),
+          AutoBuilder.followPath(PathPlannerPath.fromPathFile(autoString.substring(i - 1, i + 2)))
+        );
+
+        if(autoString.charAt(i) == 'L' || autoString.charAt(i) == 'R') {
+          autoCommand.addCommands(
+            Robot.pivot.unstowCommand(),
+            RobotContainer.shooter.indexAndShootCommand(() -> Units.RadiansPerSecond.of(0.0)).withTimeout(6.0) //Change 6 secs to how much time it takes to shoot all fuel plus change the flywheel speed
+          );
+        }
+        currentPos = autoString.charAt(i);
       } catch (Exception e) {
 
       }
--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 126;
-  public static final String GIT_SHA = "f42e46756fb76419920b3d2e2bc86b91f9f0df3f";
-  public static final String GIT_DATE = "2026-02-17 16:37:48 EST";
+  public static final int GIT_REVISION = 154;
+  public static final String GIT_SHA = "48f823820a9206c83ffea94537a058c1336d1e09";
+  public static final String GIT_DATE = "2026-02-18 15:14:04 EST";
   public static final String GIT_BRANCH = "new-auto";
-  public static final String BUILD_DATE = "2026-02-18 15:12:31 EST";
-  public static final long BUILD_UNIX_TIME = 1771445551322L;
+  public static final String BUILD_DATE = "2026-02-18 17:06:50 EST";
+  public static final long BUILD_UNIX_TIME = 1771452410652L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
