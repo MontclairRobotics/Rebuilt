3e0db30e51947390acac229cd8540c45fbb5057e
Omega314OB <brackenburyoj@gmail.com>
2026-02-19 15:33:17 -0500

    These Zones Are Dynamic
    Updated zones to be dynamic based on robot velocity

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 138;
-  public static final String GIT_SHA = "42f31f73033f08d068fc06a75ef614d5d47098e8";
-  public static final String GIT_DATE = "2026-02-17 01:10:57 EST";
+  public static final int GIT_REVISION = 140;
+  public static final String GIT_SHA = "14184f0a216e01f3d568a02c316adb9d11ce9bcc";
+  public static final String GIT_DATE = "2026-02-18 16:47:46 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-02-18 16:19:06 EST";
-  public static final long BUILD_UNIX_TIME = 1771449546844L;
+  public static final String BUILD_DATE = "2026-02-19 15:22:36 EST";
+  public static final long BUILD_UNIX_TIME = 1771532556502L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
