e0a2934996518fe88e0d0e4f3700eb00c422cdf2
Bry <155580880+BCODERW@users.noreply.github.com>
2026-02-21 11:07:26 -0500

    Auto Works (kinda)
    The auto parser itself fully works, but the paths themselves have issues and we don't have the NetworkTable stuff working yet

--- src/main/java/frc/robot/Auto.java
+++ src/main/java/frc/robot/Auto.java
@@ -1,94 +1,68 @@
+//TODO: Send why there is an error
+
 package frc.robot;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.PathPlannerPath;
 
-import edu.wpi.first.networktables.BooleanEntry;
-import edu.wpi.first.networktables.BooleanTopic;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StringEntry;
+import edu.wpi.first.networktables.StringPublisher;
 import edu.wpi.first.networktables.StringTopic;
-import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.constants.PivotConstants;
 
 public class Auto extends SubsystemBase {
-  private String[] pathNames =
-      new String[] {
-        "LD0", "LD1", "LD2", "LL0", "LL1", "LL2", "RO0", "RO1", "RO2", "RO3", "RR0", "RR1", "RR2", "LR0", "RL0"
-      };
   private char currentPos;
   private double timeToEmptyFuel = 6.0; //TODO: get the actual value
-  private int maxFuel = 15; //TODO: get the actual value
+  private double maxFuel = 24.0; //TODO: get the actual value
   private NetworkTableInstance inst = NetworkTableInstance.getDefault();
-  private NetworkTable autoTable = inst.getTable("Auto");
-  private StringTopic autoTopic = autoTable.getStringTopic("Auto String");
-  private BooleanTopic pushAutoTopic = autoTable.getBooleanTopic("Push Auto");
-  private StringEntry autoPathEntry = autoTopic.getEntry("");
-  private BooleanEntry pushAutoEntry = pushAutoTopic.getEntry(false);
+  private NetworkTable auto = inst.getTable("Auto");
+  private StringTopic testTopic = auto.getStringTopic("Auto String");
+  private StringPublisher testPub = testTopic.publish();
+  StringEntry stringEnt = testTopic.getEntry("");
+
+  // private NetworkTable autoTable;
+  // private StringTopic autoTopic;
+  // private StringPublisher autoPub;
+  // private BooleanTopic pushAutoTopic = autoTable.getBooleanTopic("Push Auto");
+  // private StringEntry autoPathEntry = autoTopic.getEntry("");
+  // private BooleanEntry pushAutoEntry = pushAutoTopic.getEntry(false);
+
   private Command autoCommand;
+  private StringPublisher errorPub;
 
   public Auto() {
-    //Ex auto: LL0L1D0
-  }
-
-  public int calculateMaxScore(String autoString) {
-    int estimatedScore = 0;
-    if(! isAutoValid(autoString)) {
-      return - 1;
-    }
-
-    if(autoString.substring(0,2) != "LR" && autoString.substring(0,2) != "RL") {
-      estimatedScore += maxFuel; //Assume we collect as much fuel as we can score at for every path + get all fuel in. Change 15 to the actual max fuel storage number
-    }
-
-    for(int i = 3; i < autoString.length(); i += 2) {
-      if(autoString.substring(i,i + 2) != "LR" && autoString.substring(i,i + 2) != "RL") {
-        estimatedScore += maxFuel; //Assume we collect as much fuel as we can score at for every path + get all fuel in. Change 15 to the actual max fuel storage number
-      }
-    }
-
-    return estimatedScore;
+    // stringEnt.set("");
   }
 
   public boolean isAutoValid(String autoString) {
-    if (autoString.length() < 5 || autoString.length() % 2 == 0) {
+    if(autoString.length() < 3) {
+      // errorPub.set("Auto length is too short");
+    }
+
+    if(autoString.length() % 2 == 0) {
+      // errorPub.set("One of the paths has too many or few characters");
       return false;
     }
 
     String currentAutoString = autoString.substring(0,3);
-    boolean doesPathExist = false;
-    for(int i = 0; i < autoString.length(); i++) {
-      if(currentAutoString == pathNames[i]) {
-        doesPathExist = true;
-      }
+    try {
+      AutoBuilder.followPath(PathPlannerPath.fromPathFile(currentAutoString));
+    } catch(Exception e) {
+      // errorPub.set("Path " + currentAutoString + " doesn\'t exist");
     }
 
-    if(! doesPathExist) {
-      return false;
-    }
-
-    currentPos = autoString.charAt(1);
     for(int i = 3; i < autoString.length(); i += 2) {
-      currentAutoString = currentPos + autoString.substring(i, i + 2);
-      doesPathExist = false;
-      for(int j = 0; j < pathNames.length; j++) {
-        if(currentAutoString == pathNames[j]) {
-          doesPathExist = true;
-        }
-
-        currentPos = currentAutoString.charAt(1);
-      }
-
-      if(! doesPathExist) {
-        return false;
+      try {
+        AutoBuilder.followPath(PathPlannerPath.fromPathFile(currentAutoString));
+      } catch(Exception e) {
+        // errorPub.set("Path " + currentAutoString + "doesn\'t exist");
       }
     }
 
@@ -96,95 +70,112 @@ public class Auto extends SubsystemBase {
   }
 
   public Command buildAuto(String autoString) {
-    if (!isAutoValid(autoString)) {
-      return RobotContainer.shooter.indexAndShootCommand(() -> Units.RadiansPerSecond.of(0.0)).withTimeout(timeToEmptyFuel * (8.0 / 24.0)); //TODO: Get max amount of fuel
-    }
 
+    //RR0R1
     SequentialCommandGroup autoCommand = new SequentialCommandGroup();
 
+    if (!isAutoValid(autoString)) {
+      try {
+        AutoBuilder.followPath(PathPlannerPath.fromPathFile("LL0"));
+        return autoCommand;
+      } catch(Exception e) {
+
+      }
+
+    }
+
+    //Valid
+
     currentPos = autoString.charAt(1);
+    //'R'
+
     try {
       if(currentPos != 'D' || autoString.charAt(2) == '0' && currentPos != 'O' || autoString.charAt(2) == '0') {
+        //Runs
         if(DriverStation.getAlliance().get() == Alliance.Blue) {
           autoCommand.addCommands(
-            RobotContainer.pivot.goToAngleCommand(PivotConstants.MIN_ANGLE),
+            //RobotContainer.pivot.goToAngleCommand(PivotConstants.MIN_ANGLE),
             Commands.race(
-                AutoBuilder.followPath(PathPlannerPath.fromPathFile(autoString.substring(0, 3))),
-                RobotContainer.rollers.intakeCommand()
+              AutoBuilder.followPath(PathPlannerPath.fromPathFile(autoString.substring(0, 3)))
+              //Follow path: RR0
+              //RobotContainer.rollers.intakeCommand()
             )
           );
         } else {
           autoCommand.addCommands(
-            RobotContainer.pivot.goToAngleCommand(PivotConstants.MIN_ANGLE),
+            // RobotContainer.pivot.goToAngleCommand(PivotConstants.MIN_ANGLE),
             Commands.race(
-              AutoBuilder.followPath(PathPlannerPath.fromPathFile(autoString.substring(0, 3)).flipPath()),
-              RobotContainer.rollers.intakeCommand()
+              AutoBuilder.followPath(PathPlannerPath.fromPathFile(autoString.substring(0, 3)).flipPath())
+              //RobotContainer.rollers.intakeCommand()
             )
           );
         }
 
         if(autoString.charAt(1) == 'L' || autoString.charAt(1) == 'R' && autoString.substring(0,2) != "LR" && autoString.substring(0,2) != "RL") {
           autoCommand.addCommands(
-            RobotContainer.pivot.goToAngleCommand(PivotConstants.MAX_ANGLE),
-            RobotContainer.shooter.indexAndShootCommand(() -> Units.RadiansPerSecond.of(0.0)).withTimeout(timeToEmptyFuel)
+            //RobotContainer.pivot.goToAngleCommand(PivotConstants.MAX_ANGLE),
+            //RobotContainer.shooter.indexAndShootCommand(() -> Units.RadiansPerSecond.of(0.0)).withTimeout(timeToEmptyFuel)
           );
         }
       } else {
         if(DriverStation.getAlliance().get() == Alliance.Blue) {
           autoCommand.addCommands(
-            RobotContainer.pivot.goToAngleCommand(PivotConstants.MIN_ANGLE),
+            // RobotContainer.pivot.goToAngleCommand(PivotConstants.MIN_ANGLE),
             Commands.race(
-              AutoBuilder.followPath(PathPlannerPath.fromPathFile(autoString.substring(0, 3))),
-              Commands.sequence(
-                Commands.waitUntil(() -> Superstructure.isInScoringZone()),
-                RobotContainer.pivot.goToAngleCommand(PivotConstants.MAX_ANGLE)
+              AutoBuilder.followPath(PathPlannerPath.fromPathFile(autoString.substring(0, 3)))
+              //Commands.sequence(
+                // Commands.waitUntil(() -> Superstructure.isInScoringZone()),
+                // RobotContainer.pivot.goToAngleCommand(PivotConstants.MAX_ANGLE)
                 //RobotContainer.shooter.whileScoring() //IDK how they have implemented scoring while dirving so this is a placeholder
-              ),
-              RobotContainer.rollers.intakeCommand()
+              //),
+              // RobotContainer.rollers.intakeCommand()
             )
           );
         } else {
           autoCommand.addCommands(
-              RobotContainer.pivot.goToAngleCommand(PivotConstants.MIN_ANGLE),
-              Commands.race(
-                AutoBuilder.followPath(PathPlannerPath.fromPathFile(autoString.substring(0, 3)).flipPath()),
-                Commands.sequence(
-                  Commands.waitUntil(() -> Superstructure.isInScoringZone()),
-                  RobotContainer.pivot.goToAngleCommand(PivotConstants.MAX_ANGLE)
-                  // RobotContainer.shooter.whileScoring().until(() -> !Superstructure.isInScoringZone()) IDK how they have implemented scoring while dirving so this is a placeholder
-                ),
-                RobotContainer.rollers.intakeCommand()
-              )
-            );
+           // RobotContainer.pivot.goToAngleCommand(PivotConstants.MIN_ANGLE),
+            Commands.race(
+              AutoBuilder.followPath(PathPlannerPath.fromPathFile(autoString.substring(0, 3)).flipPath())
+              /*Commands.sequence(
+                // Commands.waitUntil(() -> Superstructure.isInScoringZone()),
+                RobotContainer.pivot.goToAngleCommand(PivotConstants.MAX_ANGLE)
+                // RobotContainer.shooter.whileScoring().until(() -> !Superstructure.isInScoringZone()) IDK how they have implemented scoring while dirving so this is a placeholder
+              ),
+              RobotContainer.rollers.intakeCommand()*/
+            )
+          );
         }
       }
     } catch (Exception e) {
-
+      // errorPub.set("Unknown error");
     }
 
     for (int i = 3; i < autoString.length(); i += 2) {
       try {
         if(DriverStation.getAlliance().get() == Alliance.Blue) {
           autoCommand.addCommands(
-            RobotContainer.pivot.goToAngleCommand(PivotConstants.MIN_ANGLE),
-            AutoBuilder.followPath(PathPlannerPath.fromPathFile(autoString.substring(i - 1, i + 2)))
+            // RobotContainer.pivot.goToAngleCommand(PivotConstants.MIN_ANGLE),
+            //i = 3
+            //
+            AutoBuilder.followPath(PathPlannerPath.fromPathFile(currentPos + autoString.substring(i, i + 2)))
           );
         } else {
           autoCommand.addCommands(
-            RobotContainer.pivot.goToAngleCommand(PivotConstants.MIN_ANGLE),
-            AutoBuilder.followPath(PathPlannerPath.fromPathFile(autoString.substring(i - 1, i + 2)).flipPath())
+            // RobotContainer.pivot.goToAngleCommand(PivotConstants.MIN_ANGLE),
+            AutoBuilder.followPath(PathPlannerPath.fromPathFile(currentPos + autoString.substring(i, i + 2)).flipPath())
           );
         }
 
+        currentPos = autoString.charAt(i);
         if(autoString.charAt(i) == 'L' || autoString.charAt(i) == 'R') {
           autoCommand.addCommands(
-            RobotContainer.pivot.goToAngleCommand(PivotConstants.MAX_ANGLE),
-            RobotContainer.shooter.indexAndShootCommand(() -> Units.RadiansPerSecond.of(0.0)).withTimeout(timeToEmptyFuel)
+            // RobotContainer.pivot.goToAngleCommand(PivotConstants.MAX_ANGLE),
+            // RobotContainer.shooter.indexAndShootCommand(() -> Units.RadiansPerSecond.of(0.0)).withTimeout(timeToEmptyFuel)
           );
         }
         currentPos = autoString.charAt(i);
       } catch (Exception e) {
-
+        // errorPub.set("Unknown issue");
       }
     }
 
@@ -195,16 +186,19 @@ public class Auto extends SubsystemBase {
     autoCommand = buildAuto(autoString);
   }
 
-  public Command getAutoCommand() {
-        return autoCommand;
+  public Command getAutoCommand(String str) {
+    return buildAuto(str);
   }
 
-  public void periodic() {
-    if(DriverStation.isDisabled()) {
-      String autoString = autoPathEntry.get("");
-      SmartDashboard.putNumber("Score", calculateMaxScore(autoString));
 
-      createAuto(autoString);
-    }
+  public void periodic() {
+    // createAuto("LL0");
+    //testPub.set(stringEnt.get());
+    // if(DriverStation.isDisabled()) {
+      // String autoString = autoPathEntry.get("");
+      // if(pushAutoEntry.get()) {
+        // createAuto(autoString);
+      // }
+    // }
   }
 }
