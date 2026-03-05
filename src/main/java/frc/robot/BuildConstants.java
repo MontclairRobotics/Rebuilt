210185d7270f74d64deddeae2492323c33ac1516
Bry <155580880+BCODERW@users.noreply.github.com>
2026-03-05 17:07:36 -0500

    Fixed the "fixed" auto paths

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 278;
-  public static final String GIT_SHA = "eca9c2915cf6a5eba1c8f6a0c12a4ecec34ade27";
-  public static final String GIT_DATE = "2026-03-05 10:34:20 EST";
-  public static final String GIT_BRANCH = "everything-will-work-trust!";
-  public static final String BUILD_DATE = "2026-03-05 11:28:52 EST";
-  public static final long BUILD_UNIX_TIME = 1772728132137L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 280;
+  public static final String GIT_SHA = "08701d31e15771163d2e4d0a565ecf4b1f02a22a";
+  public static final String GIT_DATE = "2026-03-05 15:02:11 EST";
+  public static final String GIT_BRANCH = "superstructure-refactoring";
+  public static final String BUILD_DATE = "2026-03-05 16:34:41 EST";
+  public static final long BUILD_UNIX_TIME = 1772746481657L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
