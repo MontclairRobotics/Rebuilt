a61da569c6f58579e285ac4e97a42dca2a72dfd6
rafaelbaird <155581003+rafaelbaird@users.noreply.github.com>
2026-03-08 21:34:20 -0400

    End of Warren Hills!

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 297;
-  public static final String GIT_SHA = "b5148c933d6521c9a08885ecfb7ce6d33b0de3f3";
-  public static final String GIT_DATE = "2026-03-08 08:13:35 EDT";
+  public static final int GIT_REVISION = 298;
+  public static final String GIT_SHA = "2536327834801c20c99b35135e42f6df6b03fe28";
+  public static final String GIT_DATE = "2026-03-08 17:38:12 EDT";
   public static final String GIT_BRANCH = "warren-hills";
-  public static final String BUILD_DATE = "2026-03-08 17:15:57 EDT";
-  public static final long BUILD_UNIX_TIME = 1773004557276L;
+  public static final String BUILD_DATE = "2026-03-08 18:13:53 EDT";
+  public static final long BUILD_UNIX_TIME = 1773008033034L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
