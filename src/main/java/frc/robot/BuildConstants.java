bc576c2793e633194e60d62d8f21e9adf075eac2
Bry <155580880+BCODERW@users.noreply.github.com>
2026-03-10 15:28:57 -0400

    Slowed robot down
    The robo translation vel, accel, rotation vel, accel, were all lowered, and deadbands were increased

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 268;
-  public static final String GIT_SHA = "3bb643c88d831ec0aa31c1881a8dc8330417b20c";
-  public static final String GIT_DATE = "2026-03-03 10:53:42 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-03 22:54:28 EST";
-  public static final long BUILD_UNIX_TIME = 1772596468857L;
+  public static final int GIT_REVISION = 271;
+  public static final String GIT_SHA = "a499816ce16aa1a2814293841df65fbbe68883b9";
+  public static final String GIT_DATE = "2026-03-04 11:10:42 EST";
+  public static final String GIT_BRANCH = "outreach";
+  public static final String BUILD_DATE = "2026-03-10 15:23:10 EDT";
+  public static final long BUILD_UNIX_TIME = 1773170590776L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/drivetrain/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/CommandSwerveDrivetrain.java
@@ -296,17 +296,17 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
 	}
 
 	public double getStrafeVelocityFromController() {
-		double xInput = -MathUtil.applyDeadband(RobotContainer.driverController.getLeftX(), 0.1);
+		double xInput = -MathUtil.applyDeadband(RobotContainer.driverController.getLeftX(), 0.2);
 		return MathUtil.copyDirectionPow(xInput, JOYSTICK_INPUT_GAIN) * MAX_SPEED.in(MetersPerSecond);
 	}
 
 	public double getForwardVelocityFromController() {
-		double yInput = -MathUtil.applyDeadband(RobotContainer.driverController.getLeftY(), 0.1);
+		double yInput = -MathUtil.applyDeadband(RobotContainer.driverController.getLeftY(), 0.2);
 		return MathUtil.copyDirectionPow(yInput, JOYSTICK_INPUT_GAIN) * MAX_SPEED.in(MetersPerSecond);
 	}
 
 	public double getOmegaVelocityFromController() {
-		double rotInput = -MathUtil.applyDeadband(RobotContainer.driverController.getRightX(), 0.1);
+		double rotInput = -MathUtil.applyDeadband(RobotContainer.driverController.getRightX(), 0.2);
 		return MathUtil.copyDirectionPow(rotInput, JOYSTICK_INPUT_ROT_GAIN) * MAX_ANGULAR_SPEED.in(RadiansPerSecond);
 	}
 
