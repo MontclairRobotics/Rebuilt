3154e6d0b73c79534ae01c5364b614caa895ea69
Jame <jctee02@gmail.com>
2026-03-13 16:53:45 -0400

    Made Tuning for the Pivot Possible

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 302;
-  public static final String GIT_SHA = "3a570655b67a68cc6f4f9be48840098e70ca3031";
-  public static final String GIT_DATE = "2026-03-13 09:08:09 EDT";
+  public static final int GIT_REVISION = 306;
+  public static final String GIT_SHA = "4d966c030518981ad247ef62ed7913395cd851b4";
+  public static final String GIT_DATE = "2026-03-13 14:58:07 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-13 09:14:08 EDT";
-  public static final long BUILD_UNIX_TIME = 1773407648933L;
+  public static final String BUILD_DATE = "2026-03-13 16:48:50 EDT";
+  public static final long BUILD_UNIX_TIME = 1773434930660L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
