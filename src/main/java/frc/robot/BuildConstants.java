710ad43db895772b3d94c3ee669d89a88dfcf5f0
Bry <155580880+BCODERW@users.noreply.github.com>
2026-03-19 18:43:06 -0400

    LL5 tested
    LL5 mostly works in sim - Rafael said that it should work IRL

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 334;
-  public static final String GIT_SHA = "ea80ff7c04f7548074720ffe9fdc1a3f18cf968b";
-  public static final String GIT_DATE = "2026-03-19 18:11:36 EDT";
+  public static final int GIT_REVISION = 335;
+  public static final String GIT_SHA = "e4a4d2f6ecedd52ecf8f2b940bb3046c155ff9e5";
+  public static final String GIT_DATE = "2026-03-19 18:29:57 EDT";
   public static final String GIT_BRANCH = "rotated-auto";
-  public static final String BUILD_DATE = "2026-03-19 18:25:22 EDT";
-  public static final long BUILD_UNIX_TIME = 1773959122980L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-03-19 18:34:09 EDT";
+  public static final long BUILD_UNIX_TIME = 1773959649039L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
