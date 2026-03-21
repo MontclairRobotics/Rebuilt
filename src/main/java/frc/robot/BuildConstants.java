66cb4a7087d00807f9300906310d336b258e6ec3
cbgpenguin <cbaneygiampoala@gmail.com>
2026-03-20 22:09:55 -0400

    Hood absolute encoder offset
    The hood fell off the robort... It was really funny

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 337;
-  public static final String GIT_SHA = "c1477aae508326c58805a30c4d4b404808a5b76b";
-  public static final String GIT_DATE = "2026-03-19 21:37:28 EDT";
+  public static final int GIT_REVISION = 338;
+  public static final String GIT_SHA = "4b2860fdca57b00fbada044948f3b2c259a3a4e4";
+  public static final String GIT_DATE = "2026-03-20 18:00:53 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-20 17:28:21 EDT";
-  public static final long BUILD_UNIX_TIME = 1774042101042L;
+  public static final String BUILD_DATE = "2026-03-20 18:49:37 EDT";
+  public static final long BUILD_UNIX_TIME = 1774046977283L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
